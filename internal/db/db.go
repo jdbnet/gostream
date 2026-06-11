c442bab4842adfc0f15748dae630ae43a328576b
Jamie Banks <jamie@jdbnet.co.uk>
2026-06-11 02:19:19 +0100

    feat: enable multiStatements in database connection DSN strings

--- internal/db/db.go
+++ internal/db/db.go
@@ -54,7 +54,7 @@ type DB struct {
 }
 
 func Connect(cfg *config.Config) (*DB, error) {
-	dsnWithoutDB := fmt.Sprintf("%s:%s@tcp(%s:%d)/?parseTime=true",
+	dsnWithoutDB := fmt.Sprintf("%s:%s@tcp(%s:%d)/?parseTime=true&multiStatements=true",
 		cfg.Database.User,
 		cfg.Database.Password,
 		cfg.Database.Host,
@@ -72,7 +72,7 @@ func Connect(cfg *config.Config) (*DB, error) {
 		return nil, fmt.Errorf("failed to create database: %w", err)
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true&multiStatements=true",
 		cfg.Database.User,
 		cfg.Database.Password,
 		cfg.Database.Host,
